6b4cbffc64c43d7ce78130e5774229389bdcbc31
CaioOoliveiraGarcoa <caio.garcia@germinare.org.br>
2024-03-14 13:14:25 -0300

    tabulaçao na linha

--- Cliente.java
+++ Cliente.java
@@ -2,5 +2,5 @@ public class Cliente {
     private String nome;
     private String cpf;
     private int agencia;
-    private double saldo;
+                                private double saldo;
 }
